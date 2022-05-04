7d7b0aaf3f46c9a68895c619ac239c1737711628
Jeff Hoyt <101358655+JeffjHoyt@users.noreply.github.com>
2022-05-04 15:54:40 -0400

    Update and rename java.js to index.js

---
+++ JWD-Final-Project-main/index.js
@@ -0,0 +1,13 @@
+const newTaskForm 
+
+addEventListener(submit) {
+
+}
+
+function eventHandler(event) {
+        if (event.type == 'submit')&&(validate = true) {
+            
+        } else {
+    }
+}
+
