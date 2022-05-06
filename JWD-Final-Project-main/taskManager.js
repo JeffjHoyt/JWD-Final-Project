878e29993e47cdd702a8f40996c0c55d5445003e
Jeff Hoyt <101358655+JeffjHoyt@users.noreply.github.com>
2022-05-06 09:25:16 -0400

    task 8 Jeff Hoyt

--- JWD-Final-Project-main/taskManager.js
+++ JWD-Final-Project-main/taskManager.js
@@ -33,6 +33,46 @@ class TaskManager {
         }
         this.tasks.push(task)
     }
+    
+    //task 8 Jeff Hoyt
+    
+    render() {
+        const tasksHtmlList = [];
+        for (let i = 0; i < this.tasks.length; i++) {
+            const task = this.tasks[i];
+            const date = new Date(task.dueDate);
+            const formattedDate = date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear();
+            const taskHtml = createTaskHtml(task.id, task.name, task.description, task.assignedTo, formattedDate, task.status);
+            tasksHtmlList.push(taskHtml);
+        }
+
+        const tasksHtml = tasksHtmlList.join('\n');
+        const tasksList = document.querySelector('#tasksList');
+        tasksList.innerHTML = tasksHtml;
+    }
+
+    save() {
+        const tasksJson = JSON.stringify(this.tasks);
+        localStorage.setItem('tasks', tasksJson);
+        const currentId = String(this.currentId);
+        localStorage.setItem('currentId', currentId);
+    }
+
+    load() {
+        if (localStorage.getItem('tasks')) {
+            const tasksJson = localStorage.getItem('tasks');
+            this.tasks = JSON.parse(tasksJson);
+        }
+    }
+}
+
+const test = new TaskManager("whatever");
+
+test.addTask("Jeff", "take out the trash", "not me", "Nov, 11th" )
+
+
+
+console.log(test.tasks);
 
     // creating delete task method
     deleteTask(taskId){
