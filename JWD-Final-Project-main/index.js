6c508d734cdd73d172915a8077d94fd840e6ff41
mahamefule <chinedumike2010@gmail.com>
2022-05-05 18:13:04 -0700

    first step completed

--- JWD-Final-Project-main/index.js
+++ JWD-Final-Project-main/index.js
@@ -11,3 +11,5 @@ function eventHandler(event) {
     }
 }
 
+const taskHtml = createTaskHtml(1, "Trash take out", "take out the trash", "michael", "05/05/2022", "pass a value");
+console.log(taskHtml);
